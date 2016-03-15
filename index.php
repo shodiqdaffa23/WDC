d523b88c7d7daf5e1b318b904549d690c6f481fd
Ayat Maulana <Ayatmaulana.04@gmail.com>
2016-03-15 23:26:59 +0700

    kedua

--- index.php
+++ index.php
@@ -1,8 +1,73 @@
 <?php include 'style/header.php'; ?>
 
 	<div id="content-first">
-		<div class="left">s</div>
-		<div class="right">d</div>
+		<div class="left">
+			<div class="headline">
+				<img src="img/a.jpg" style="width:100%;height:100%" alt="">
+			<div class="caption">
+				<p>Lorem ipsum dolor sit amet, consectetur.</p>
+			</div>
+			</div>
+		</div>
+		<div class="right">
+			<div id="hot-news">
+				<h2>Hot News</h2>
+				<ul>
+					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam!</a></li>
+					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis.</a></li>
+					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At.</a></li>
+					<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit?</a></li>
+				</ul>
+			</div>
+		</div>
+		<div style="clear:both"></div>
 	</div>
+	<div id="content-second">
+		<ul class="content-container">
+			<li class="box">
+				<img src="img/konten/1 (1).jpg" alt="">
+				<h2>Lorem ipsum dolor sit amet.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, sunt.</p>
+			</li>
+			<li class="box">
+				<img src="img/konten/1 (3).jpg" alt="">
+				<h2>Lorem ipsum dolor sit amet, consectetur.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis omnis ea eum ipsam dignissimos, eligendi, aliquam consectetur nulla odit laboriosam.</p>
+			</li>
+			<li class="box">
+				<img src="img/konten/1 (9).jpg" alt="">
+				<h2>Lorem ipsum dolor.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam deleniti quaerat aperiam dolor, eos consectetur dolorum ex. Officiis, aliquid, omnis.</p>
+			</li>
+			<li class="box">
+				<img src="img/konten/1 (4).jpg" alt="">
+				<h2>Lorem ipsum dolor sit.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, blanditiis.</p>
+			</li>
+			<li class="box">
+				<img src="img/konten/1 (5).png" alt="">
+				<h2>Lorem ipsum dolor sit amet.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro deleniti dicta sequi, libero non veniam consequuntur minima.</p>
+			</li>
+			<li class="box">
+				<img src="img/konten/1 (3).png" alt="">
+				<h2>Lorem ipsum dolor sit amet.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem excepturi velit dignissimos ipsum nisi?</p>
+			</li>
+			<li class="box">
+				<img src="img/konten/1 (2).png" alt="">
+				<h2>Lorem ipsum dolor sit.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, eos dolorem velit nihil facere. Omnis.</p>
+			</li>
+			<li class="box">
 
+				<img src="img/konten/1 (1).png" alt="">
+				<h2>Lorem ipsum dolor sit amet.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, officiis.</p>
+			</li>
+		</ul>
+	</div>
+	<div id="profile">
+		profile
+	</div>
 <?php include 'style/footer.php'; ?>
